48dc01e42ab43a824b08da365ad913e832616a8f
ryan <ryandrivera@gmail.com>
2021-12-07 10:08:56 -0800

    Add project files.

---
+++ InventoryBeginners/Controllers/UnitController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InventoryBeginners.Data;
+using InventoryBeginners.Models;
+using System.Linq;
+
+namespace InventoryBeginners.Controllers
+{
+    public class UnitController : Controller
+    {
+        public IActionResult Index()//Read method of CRUD operations.  Lists all data from the units table
+        {
+            List<Unit> units = _context.Units.ToList(); //Gets the list of units from the context
+            return View(units);
+        }
+     
+        private readonly InventoryContext _context;
+
+        //Dependency Injection
+        public UnitController(InventoryContext context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ InventoryBeginners/Data/InventoryContext.cs
@@ -0,0 +1,19 @@
+﻿using InventoryBeginners.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InventoryBeginners.Data
+{
+    public class InventoryContext: DbContext
+    {
+        public InventoryContext(DbContextOptions options) :base(options)
+        {
+
+        }
+
+        public virtual DbSet<Unit> Units { get; set; }
+    }
+}
